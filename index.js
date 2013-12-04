3a598cd7ee91b1bffd7f49192817bd7aee9572ae
Tim <secoif@gmail.com>
2013-12-04 20:50:18 +1000

    Expose server/client from main export.

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ var Server = require('./server');
 var Client = require('./client');
 
 // Combine Server and Client
-exports = module.exports = function Jobs(db, worker, options) {
+exports = module.exports = Jobs;
+
+function Jobs(db, worker, options) {
   return mixin(Server(db, worker, options), Client.Queue.prototype);
 }
+
+Jobs.Server = Server;
+Jobs.Client = Client;
 
 function mixin(a, b) {
   var target = Object.create(a); // avoids modifying original
